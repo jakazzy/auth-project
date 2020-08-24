5552244fe75a1594a2021a890d33578bb0d67d48
jakazzy <jakazzy@gmail.com>
2020-08-24 17:19:54 +0000

    display message for user to login to view guides if loggedout

--- scripts/index.js
+++ scripts/index.js
@@ -1,22 +1,25 @@
 const guideList = document.querySelector(".guides");
 
 const setupGuides = (data) => {
-    if(data.length)
-  let html = "";
-  data.forEach((doc) => {
-    const guide = doc.data();
-    console.log(guide);
-    const li = `
-    <li>
-        <div class="collapsible-header grey lighten-4">${guide.title}</div>
-        <div class="collapsible-body white"><span>${guide.content}</span></div>
-    </li>
-    `;
+  if (data.length) {
+    let html = "";
+    data.forEach((doc) => {
+      const guide = doc.data();
+      console.log(guide);
+      const li = `
+            <li>
+                <div class="collapsible-header grey lighten-4">${guide.title}</div>
+                <div class="collapsible-body white"><span>${guide.content}</span></div>
+            </li>
+            `;
 
-    html += li;
-  });
+      html += li;
+    });
 
-  guideList.innerHTML = html;
+    guideList.innerHTML = html;
+  } else {
+    guideList.innerHTML = `<h5 class="center align">Login to view guides</h5>`;
+  }
 };
 // setup materialize components
 document.addEventListener("DOMContentLoaded", function () {
