f9a913361abf9c3607841f378c40872b47349096
jakazzy <jakazzy@gmail.com>
2020-08-25 03:26:39 +0000

    secure cloud functions

--- functions/index.js
+++ functions/index.js
@@ -11,6 +11,10 @@ const admin = require("firebase-admin");
 admin.initializeApp();
 
 exports.addAdminRole = functions.https.onCall((data, context) => {
+  //check request is made by an admin
+  if (context.auth.token.admin !== true) {
+    return { error: "Only admins can add other admins" };
+  }
   // get user and add custom claims (admin)
   return admin
     .auth()
